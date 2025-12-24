802e47fbc3018182a2042a059aef2c117620ab66
yamms2340 <yaminireddy2023@gmail.com>
2025-12-25 02:47:26 +0530

    cors2

--- help-platform/client/src/services/api.js
+++ help-platform/client/src/services/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL =
+  process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
 
 // Create axios instance
 const api = axios.create({
